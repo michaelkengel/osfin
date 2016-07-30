9efb57b2f8dd783896df7f7d05be91eeaa198ae5
michaelkengel <Michael@unknown.local>
2016-07-30 14:51:22 -0400

    fixed delete

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-//  MikeLite Shell v1.62
+//  MikeLite Shell v1-7.c
 //  main.c
 //  finalproj
 //  Created by Mike on 6/01/16.
@@ -48,7 +48,7 @@ int main(int argc, char** argv) {
     printf("%s\n", "|..../  /..../  /....../      /./  /.../  /...../        /....|" );
     printf("%s\n", "|....////....////......////////.////...////.....//////////....|" );
     printf("%s\n", "|-------------------------------------------------------------|" );
-    printf("%s\n", "|                     Mike-Lite Shell v1.62                   |" );
+    printf("%s\n", "|                     Mike-Lite Shell v1.7                    |" );
     printf("%s\n", "|                                                             |" );
     printf("%s\n", "|                 Operating Systems 575 SU '16                |" );
     printf("%s\n", "|_____________________________________________________________|" );
@@ -84,130 +84,129 @@ int main(int argc, char** argv) {
         // Make sure there is smething in buffer other than null term
         if(CommandLineInput[0] != '\0'){
             parseString(CommandLineInput);
-         
-        // Jump marker for no history
+            
+            // Jump marker for no history
         jumpList:
             
-        for(int index=0; (index < BUFFER_SIZE) && !(parameterList[index]==NULL); index++){
-        // Just walk and init
-        }
-        for(int index=0; (index < BUFFER_SIZE) && !(historyList[index] == NULL); index++){
-        // Just walk and init
-        }
-         
-        /*
-          
-        --------   COMMAND SELECTION   ----------
-         IMPLEMENT YOUR ADDITIONAL BUILD-IN COMMANDS BELOW USING THE FOLLOWING SYNTAX
-         
-         if((strcmp(parameterList[0], "YOUR FUNCTION") == 0){
-         Your execution block here for a hit
-         global_count++; Increment the global count THIS IS IMPORTANT FOR INDEXING
-         continue; Continue through next loop iteration
-         }
-         
-         */
+            for(int index=0; (index < BUFFER_SIZE) && !(parameterList[index]==NULL); index++){
+                // Just walk and init
+            }
+            for(int index=0; (index < BUFFER_SIZE) && !(historyList[index] == NULL); index++){
+                // Just walk and init
+            }
             
-        // EXIT
-        if((strcmp(parameterList[0], "quit") == 0) || (strcmp(CommandLineInput, "exit") ==0)){
-            printf("Goodbye!\n");
-            return(EXIT_SUCCESS);
-        }
-        
-        // CD
-        else if(strcmp(parameterList[0],"cd") == 0){
-            chdir(parameterList[1]);
-            global_count++;
-            continue;
-        }
-        
-        // DELETE
-        else if(strcmp(parameterList[0],"delete") == 0){
-            if (parameterList[1] != NULL) {
-                if (remove(parameterList[1])){
-                    errorThrow();
+            /*
+             
+             --------   COMMAND SELECTION   ----------
+             IMPLEMENT YOUR ADDITIONAL BUILD-IN COMMANDS BELOW USING THE FOLLOWING SYNTAX
+             
+             if((strcmp(parameterList[0], "YOUR FUNCTION") == 0){
+             Your execution block here for a hit
+             global_count++; Increment the global count THIS IS IMPORTANT FOR INDEXING
+             continue; Continue through next loop iteration
+             }
+             
+             */
+            
+            // EXIT
+            if((strcmp(parameterList[0], "quit") == 0) || (strcmp(CommandLineInput, "exit") ==0)){
+                printf("Goodbye!\n");
+                return(EXIT_SUCCESS);
+            }
+            
+            // CD
+            else if(strcmp(parameterList[0],"cd") == 0){
+                chdir(parameterList[1]);
+                global_count++;
+                continue;
+            }
+            
+            // DELETE
+            else if(strcmp(parameterList[0],"delete") == 0){
+                int retr;
+                retr = remove(parameterList[1]);
+                
+                if(retr == 0){
+                    printf("%s", "SYS> Deleted\n");
                 }
                 else{
-                    printf("%s", "Sucsess\n");
+                    errorThrow();
+                }
+                
+                global_count++;
+                continue;
+            }
+            
+            // TOUCH
+            else if(strcmp(parameterList[0],"touch") == 0){
+                if (parameterList[1] != NULL) {
+                    fopen(parameterList[1], "wb");
+                }
+                else {
+                    errorThrow();
+                }
+                global_count++;
+                continue;
+            }
+            
+            // !! EXECUTE LAST
+            else if(strcmp(parameterList[0],"!!") == 0){
+                char* linecopy;
+                linecopy = malloc(sizeof(char) * BUFFER_AS_INT);
+                linecopy = historyList[global_count-1];
+                if(strcmp(linecopy,"!!") == 0){
+                    printf("%s", "SYS> Chaining executions not allowed\n");
+                    goto linePrompt;
+                }
+                else {
+                    parseStringNoHist(linecopy);
+                    goto jumpList;
                 }
             }
-            else{
-                errorThrow();
-                }
-        
-        global_count++;
-        continue;
-        }
-        
-        // TOUCH
-        else if(strcmp(parameterList[0],"touch") == 0){
-            if (parameterList[1] != NULL) {
-                fopen(parameterList[1], "wb");
-                }
-            else {
-                errorThrow();
+            
+            // HISTORY
+            else if(strcmp(parameterList[0],"history") == 0){
+                allHist();
+                global_count++;
+                continue;
             }
-        global_count++;
-        continue;
-        }
-        
-        // !! EXECUTE LAST
-        else if(strcmp(parameterList[0],"!!") == 0){
-            char* linecopy;
-            linecopy = malloc(sizeof(char) * BUFFER_AS_INT);
-            linecopy = historyList[global_count-1];
-            if(strcmp(linecopy,"!!") == 0){
-                printf("%s", "Chaining executions not allowed\n");
-                goto linePrompt;
+            
+            // !8
+            else if(parameterList[0][0] == '!' && (parameterList[0][1] >= '0' && parameterList[0][1] <= BUFFER_AS_INT)){
+                // parse number from command
+                for(int i = 0; i < BUFFER_SIZE; i++ ){
+                    parameterList[0][i] = parameterList[0][i+1];
                 }
-            else {
+                // take input and insert
+                int k;
+                // atoi() is ASCII to int
+                k = atoi(parameterList[0]);
+                char* linecopy;
+                linecopy = malloc(sizeof(char) * BUFFER_AS_INT);
+                linecopy = historyList[k-1];
                 parseStringNoHist(linecopy);
                 goto jumpList;
             }
-        }
-        
-        // HISTORY
-        else if(strcmp(parameterList[0],"history") == 0){
-            allHist();
-            global_count++;
-            continue;
-        }
-        
-        // !8
-        else if(parameterList[0][0] == '!' && (parameterList[0][1] >= '0' && parameterList[0][1] <= BUFFER_AS_INT)){
-            // parse number from command !87
-            for(int i = 0; i < BUFFER_SIZE; i++ ){
-                parameterList[0][i] = parameterList[0][i+1];
+            
+            // HELP
+            else if(strcmp(parameterList[0],"help")== 0){
+                printf("\nWelcome to MIKE SHELL\n");
+                printf("_______________________\n");
+                printf("Built-ins: \n");
+                printf("\'exit\' - Exit the shell \n");
+                printf("\'cd\' - Change directory \n");
+                printf("\'delete [file]\' - Delete file/folder \n");
+                printf("\'touch [param]\' - Touch parameter, if non exists, create \n");
+                printf("\'!!\' - Execute last command \n");
+                printf("\'history\' - View input history \n");
+                printf("\'!n\' - Execute n'th command (use history command first) \n");
+                global_count++;
+                continue;
             }
-            // take input and insert
-            int k;
-            k = atoi(parameterList[0]);
-            char* linecopy;
-            linecopy = malloc(sizeof(char) * BUFFER_AS_INT);
-            linecopy = historyList[k-1];
-            parseStringNoHist(linecopy);
-            goto jumpList;
-        }
-           
-        // HELP
-        else if(strcmp(parameterList[0],"help")== 0){
-            printf("\nWelcome to MIKE SHELL\n");
-            printf("_______________________\n");
-            printf("Built-ins: \n");
-            printf("\'exit\' - Exit the shell \n");
-            printf("\'cd\' - Change directory \n");
-            printf("\'delete [file]\' - Delete file/folder \n");
-            printf("\'touch [param]\' - Touch parameter, if non exists, create \n");
-            printf("\'!!\' - Execute last command \n");
-            printf("\'history\' - View input history \n");
-            printf("\'!n\' - Execute n'th command (use history command first) \n");
+            
+            // GO TO EXECUTE IF NONE HIT
+            execute();
             global_count++;
-            continue;
-        }
-        
-        // GO TO EXECUTE IF NONE HIT
-        execute();
-        global_count++;
         }
     }
 }
@@ -237,7 +236,7 @@ void parseStringNoHist(char* inputLine){
 // NEED TO CHANGE
 int execute(){
     pid_t pid, wpid;
-    int status;
+    int isRunning;
     pid = fork();
     if (pid == 0){ // Child process
         if (execvp(parameterList[0], parameterList) == -1){
@@ -254,17 +253,17 @@ int execute(){
             // The status of any child processes specified by pid that are stopped,
             // and whos status has not yet been reported since they stopped,
             // shall also be reported to the requesting process.
-            wpid = waitpid(pid, &status, WUNTRACED);
+            wpid = waitpid(pid, &isRunning, WUNTRACED);
             // While there is no exited (WIFEXITED) or terminated by signal (WIFSIGNALED)
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+        } while (!WIFEXITED(isRunning) && !WIFSIGNALED(isRunning));
     }
     return 1;
 }
 
 // Genertic error throwing function
 void errorThrow(){
-    // Print the error
-    perror("Error: ");
+    // Print the stored error code as text
+    perror("SYS> Error: ");
 }
 
 // Print all the history
@@ -279,9 +278,5 @@ void allHist(){
     }
     printf("%s \n"," ---- COMPLETE ---- ") ;
 }
-
-
-
-
 
 
